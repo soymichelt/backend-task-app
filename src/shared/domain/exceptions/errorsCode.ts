aab74c2d10d620f005ef6861a21175039d08ecc6
SoymichelDev <mtraatabladaa94@gmail.com>
2024-10-19 14:19:37 -0600

    feat: add errors code to shared kernel

---
+++ src/shared/domain/exceptions/errorsCode.ts
@@ -0,0 +1,19 @@
+export const ERROR_CODES = {
+  ArgRequiredException: 'SHA-1001',
+  ArgInvalidException: 'SHA-1002',
+  IdInvalidException: 'SHA-1003',
+  InvalidDateException: 'SHA-1004',
+  GuidInvalidException: 'SHA-1005',
+  EnumValueIsInvalidException: 'SHA-1006',
+  // Users Codes
+  EmailIsInvalidException: 'USR-2001',
+  EmailAlreadyExistsException: 'USR-2002',
+  CreateUserAuthException: 'USR-2003',
+  GetUserAuthException: 'USR-2004',
+  UserAuthDisabledException: 'USR-2005',
+  UserTokenInvalidException: 'USR-2006',
+  GetUserAuthByTokenException: 'USR-2007',
+  UserTokenRequiredException: 'USR-2008',
+  // Tasks Codes
+  TaskNotFoundException: 'TSK-3001',
+};
